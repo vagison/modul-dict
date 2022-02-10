a0630a2c4006e9e40206de24e9fea6a758ab3ccb
unknown <vagison@gmail.com>
2022-02-11 01:10:39 +0400

    Trying fix cookies

--- src/components/AddTranslation/AddTranslation.js
+++ src/components/AddTranslation/AddTranslation.js
@@ -19,36 +19,6 @@ const initialState = {
   status: 0,
 };
 
-var x = (inputValue, callback) => {
-  fetch("https://modul-dictionary-api.herokuapp.com/search-field", {
-    method: "post",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      searchedField: inputValue,
-    }),
-  })
-    .then((resp) => {
-      return resp.json();
-    })
-    .then((data) => {
-      const tempArray = [];
-      if (data) {
-        if (data.length) {
-          data.forEach((element) => {
-            tempArray.push({
-              label: element.field,
-              value: element.id,
-            });
-          });
-        }
-      }
-      callback(tempArray);
-    })
-    .catch((error) => {
-      console.log(error, "error occurred");
-    });
-};
-
 class AddTranslation extends React.Component {
   constructor() {
     super();
@@ -67,7 +37,35 @@ class AddTranslation extends React.Component {
 
   // // part of speech
   // search part of speech
-  searchPOS = x
+  searchPOS = (inputValue, callback) => {
+    fetch("https://modul-dictionary-api.herokuapp.com/search-pos", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        searchedPOS: inputValue,
+      }),
+    })
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        const tempArray = [];
+        if (data) {
+          if (data.length) {
+            data.forEach((element) => {
+              tempArray.push({
+                label: element.pos,
+                value: element.id,
+              });
+            });
+          }
+        }
+        callback(tempArray);
+      })
+      .catch((error) => {
+        console.log(error, "error occurred");
+      });
+  };
 
   // change part of speech
   onPOSChange = (currentlySelectedPOS) => {
