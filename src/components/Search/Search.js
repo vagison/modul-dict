c862b3e05dcc3088dd18d405fdc244e0bf969087
unknown <vagison@gmail.com>
2022-02-16 02:20:37 +0400

    Fixing small screens

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -23,37 +23,37 @@ class Search extends React.Component {
           searchedWord: inputValue,
         }),
       })
-      .then((resp) => {
-        return resp.json();
-      })
-      .then((data) => {
-        this.props.setDirection(data["direction"]);
-        return data
-      })
-      .then((data) => {
-        const tempArray = [];
-        if (data["collection"]) {
-          if (data["collection"].length !== 0) {
-            data["collection"].forEach((element) => {
-              tempArray.push({
-                type: element.type,
-                label: element.label,
-                value: element.ids,
+        .then((resp) => {
+          return resp.json();
+        })
+        .then((data) => {
+          this.props.setDirection(data["direction"]);
+          return data;
+        })
+        .then((data) => {
+          const tempArray = [];
+          if (data["collection"]) {
+            if (data["collection"].length !== 0) {
+              data["collection"].forEach((element) => {
+                tempArray.push({
+                  type: element.type,
+                  label: element.label,
+                  value: element.ids,
+                });
               });
-            });
+            }
           }
-        }
-        callback(tempArray);
-      })
-      .catch((error) => {
-        console.log(error, "error occurred");
-      });
+          callback(tempArray);
+        })
+        .catch((error) => {
+          console.log(error, "error occurred");
+        });
     }
   };
 
   onWordSelect = (selectedWord) => {
     if (selectedWord !== "") {
-      this.setState({selectedWord});
+      this.setState({ selectedWord });
       this.props.setSearchedWord(selectedWord);
     }
   };
@@ -83,11 +83,15 @@ class Search extends React.Component {
       <article className="br3 ba b--black-10 pa3-ns mv2 w-90 w-80-m w-60-l mw7 shadow-5 center searchBox">
         <div className="pt4 pb2 ph5 ph6-ns mw6 center">
           <Logo />
-        </div>          
+        </div>
         <main className="pa3 mw6 center black-60">
           {/* <h1 className="f3 f1-ns fw6">{title}</h1> */}
           <AsyncSelect
-            components={{ LoadingIndicator: null }} 
+            components={{
+              LoadingIndicator: null,
+              DropdownIndicator: () => null,
+              IndicatorSeparator: () => null,
+            }}
             className="input-reset ba bg-transparent w-100 asyncSelectField"
             value={""}
             loadOptions={this.searchWord}
