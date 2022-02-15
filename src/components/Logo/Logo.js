a1e77c8b748ebc2e74fced90e37cd37489f0f195
unknown <vagison@gmail.com>
2022-02-16 00:51:07 +0400

    Fixing small screens

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,11 +2,7 @@ import logo from "./logo.png";
 import "./Logo.css";
 
 const ModulLogo = () => {
-  return (
-    <div className="pt4 pb2 ph5 ph6-ns mw6 center">
-      <img alt="logo" src={logo} />
-    </div>
-  );
+  return <img alt="logo" src={logo} />;
 };
 
 export default ModulLogo;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -81,8 +81,10 @@ class Search extends React.Component {
 
     return (
       <article className="br3 ba b--black-10 pa3-ns mv2 w-90 w-80-m w-60-l mw7 shadow-5 center searchBox">
-        <Logo/>
-        <main className="pa3 mw7 center black-60">
+        <div className="pt4 pb2 ph5 ph6-ns mw6 center">
+          <Logo />
+        </div>          
+        <main className="pa3 mw6 center black-60">
           {/* <h1 className="f3 f1-ns fw6">{title}</h1> */}
           <AsyncSelect
             components={{ LoadingIndicator: null }} 
